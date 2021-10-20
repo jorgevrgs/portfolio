12dea093b2d694ecc967b09c1187f9ce4064bc02
Jorge Vargas <jorgevargasla@gmail.com>
2021-10-20 15:17:00 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { src, dest, series, task, watch } = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+// const uglify = require("gulp-uglifycss");
+
+// exports.default = function () {
+//   return src("src/**/*.css").pipe(autoprefixer()).pipe(dest("css"));
+// };
+
+task("styles", async function () {
+  src("src/**/*.css")
+    .pipe(autoprefixer())
+    // .pipe(
+    //   uglify({
+    //     maxLineLen: 80,
+    //     uglyComments: true,
+    //   })
+    // )
+    .pipe(dest("css"));
+});
+
+task("watch", async function () {
+  watch("src/**/*.css", series("styles"));
+});
