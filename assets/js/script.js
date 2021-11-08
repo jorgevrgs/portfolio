3ed928d13f4e93c560acb3aa42efe86a58cfbe35
william.rolando88 <william.rolando88@hotmail.com>
2021-11-08 17:49:21 -0500

    Add menu mobile script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
-/**
- *
- */
+const header = document.querySelector('header');
+const menuButton = document.querySelector('.btn-menu');
+const menuLinks = document.querySelectorAll('.nav-link');
+
+const toggleMenu = () =>{
+  header.classList.toggle('header-mobile');
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener('click', toggleMenu);
+})
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth>992){
+    header.classList.remove('header-mobile');
+  }
+})
