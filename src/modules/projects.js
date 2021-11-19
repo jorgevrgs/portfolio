c28a67f9d3481c57c1168bc942a2f2f89a8f7283
Jorge Vargas <jorgevargasla@gmail.com>
2021-11-19 07:41:38 -0500

    Add jsdocs

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,5 +1,17 @@
 // @ts-check
 
+/**
+ * @typedef {({
+ *  id: number,
+ *  title: string,
+ *  description: string,
+ *  image: string,
+ *  technologies: string[],
+ *  linkToLive: string,
+ *  linkToSource: string,
+ * })} ProjectDef
+ */
+
 import ApiClass from '../classes/ApiClass.js';
 
 import {
@@ -15,73 +27,121 @@ export default async function projects() {
 
   // PROJECTS
   const api = new ApiClass();
+
+  /** @type {ProjectDef[]} */
   const projects = await api.get('projects');
+
+  /** @type {HTMLElement} */
   const projectsElement = document.querySelector('.projects');
 
   // ░█▀▀▀ █──█ █▀▀▄ █▀▀ ▀▀█▀▀ ─▀─ █▀▀█ █▀▀▄ █▀▀
   // ░█▀▀▀ █──█ █──█ █── ──█── ▀█▀ █──█ █──█ ▀▀█
   // ░█─── ─▀▀▀ ▀──▀ ▀▀▀ ──▀── ▀▀▀ ▀▀▀▀ ▀──▀ ▀▀▀
 
+  /**
+   *
+   * @param {HTMLElement} parentElement
+   * @param {HTMLElement} childElement
+   */
+  const appendChild = (parentElement, childElement) => {
+    if (parentElement && childElement) {
+      parentElement.appendChild(childElement);
+    }
+  };
+
+  /**
+   *
+   * @param {HTMLElement} modalElement
+   */
   const handleCloseModalButton = (modalElement) => {
-    const element = modalElement.querySelector('.modal-close');
+    if (modalElement) {
+      const element = modalElement.querySelector('.modal-close');
 
-    element.addEventListener('click', () => modalElement.remove());
+      element.addEventListener('click', () => modalElement.remove());
+    }
   };
 
+  /**
+   *
+   * @param {HTMLElement} modalElement
+   * @param {ProjectDef} project
+   */
   const handleNextModalButton = (modalElement, project) => {
-    const element = modalElement.querySelector('.btn-next');
+    if (modalElement) {
+      const element = modalElement.querySelector('.btn-next');
 
-    element.addEventListener('click', () => {
-      const findIndex = projects.findIndex((p) => p.id === project.id);
-      const lastIndex = projects.lastIndex();
+      element.addEventListener('click', () => {
+        const findIndex = projects.findIndex((p) => p.id === project.id);
+        const lastIndex = projects.length - 1;
 
-      if (findIndex === lastIndex) {
-        // not next
-      }
-    });
+        if (findIndex === lastIndex) {
+          // not next
+        }
+      });
+    }
   };
 
+  /**
+   *
+   * @param {HTMLElement} modalElement
+   * @param {ProjectDef} project
+   */
   const handlePreviousModalButton = (modalElement, project) => {
-    const element = modalElement.querySelector('.btn-prev');
+    if (modalElement) {
+      const element = modalElement.querySelector('.btn-prev');
 
-    element.addEventListener('click', () => {
-      const findIndex = projects.findIndex((p) => p.id === project.id);
-      const firstIndex = projects.firstIndex();
+      element.addEventListener('click', () => {
+        const findIndex = projects.findIndex((p) => p.id === project.id);
+        const firstIndex = 0;
 
-      if (findIndex === firstIndex) {
-        // not previous
-      }
-    });
+        if (findIndex === firstIndex) {
+          // not previous
+        }
+      });
+    }
   };
 
+  /**
+   *
+   * @param {HTMLElement} projectTemplateElement
+   * @param {ProjectDef} project
+   */
   const handleSeeProjectButton = (projectTemplateElement, project) => {
     // Add event listener to open modal
-    const element = projectTemplateElement.querySelector('.see-project');
+    if (projectTemplateElement) {
+      const element = projectTemplateElement.querySelector('.see-project');
 
-    element.addEventListener('click', () => {
-      const object = modalTemplate(project);
-      const template = buildTemplate(object);
+      element.addEventListener('click', () => {
+        const object = modalTemplate(project);
+        const template = buildTemplate(object);
 
-      projectsElement.appendChild(template);
+        appendChild(projectsElement, template);
 
-      handleCloseModalButton(template);
-      handleNextModalButton(template, project);
-      handlePreviousModalButton(template, project);
-    });
+        handleCloseModalButton(template);
+        handleNextModalButton(template, project);
+        handlePreviousModalButton(template, project);
+      });
+    }
   };
 
+  /**
+   *
+   * @param {ProjectDef} project
+   */
   const createProjectTemplate = (project) => {
     const object = projectTemplate(project);
     const template = buildTemplate(object);
 
-    // Add a project id class
-    template.classList.add(`project-${project.id}`);
+    if (template) {
+      // Add a project id class
+      template.classList.add(`project-${project.id}`);
 
-    // Insert into the container
-    projectsElement.appendChild(template);
+      // Insert into the container
+      projectsElement.appendChild(template);
 
-    // Add event listener for the click on see project
-    handleSeeProjectButton(template, project);
+      // Add event listener for the click on see project
+      handleSeeProjectButton(template, project);
+    }
   };
 
   // MAIN
